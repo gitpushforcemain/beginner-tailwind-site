54fcc2dfd192969ae80a4d1238e17e4559a7c0f6
Andrei Bogomja <bogomja@gmail.com>
2021-05-28 11:44:30 +0100

    Initial commit. Quick styles for deploy

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+const colors = require("tailwindcss/colors");
+
+module.exports = {
+  purge: [
+    "./index.html"
+  ],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        orange: colors.orange,
+        teal: colors.teal
+      }
+    }
+  },
+  variants: {
+    extend: {}
+  },
+  plugins: []
+};
